b9d849caf233c2cd9d2d8931d3585e72a21f4084
KonstantinSuvorov <106460617+KonstantinSuvorov@users.noreply.github.com>
2023-10-28 11:39:54 +0300

    Update Main.java
    Внесение информации о студентах

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,32 @@
-package HW6;
-
-//- Создать класс Юзер и перенести его в базовые поля (ФИО)
-//        - Модифицировать ранее созданный класс СтудентКомпаратор
-//        - Переименовать в ЮзерКомпаратор
-//        - Типизировать его T наследующегося от типа Юзер
-//        - Типизировать реализуемый интерфейс Компаратор T
-//        - Изменить метод компаре, внеся во входные данные вместо конкретных классов типизацию T
-//        - Внести правки места, где использовался предыдущий компаратор
-
-import HW6.Controller.GroupController;
-import HW6.Data.User;
-import HW6.Data.UserComparator;
-import HW6.Data.UserInterface;
-import HW6.View.View;
+import Controller.GroupController;
+import Data.User;
+import Data.UserComparator;
+import Data.UserInterface;
+import View.View;
 
 import java.util.TreeSet;
 
-//Взять реализованный код в рамках семинара 4 и продемонстрировать применение принципов, усвоенных на семинаре.
-//Нужно в проекте прокомментировать участки кода, которые рефакторим, какой принцип применяем и почему.
 public class Main {
     public static void main(String[] args) {
         GroupController<User> groupController = new GroupController<>();
 
+        User student1 = new User("Сeргей", "Иванович", "Иванов");
+        User student2 = new User("Петр", "Петрович", "Власов");
+        User student3 = new User("Сергей", "Иванович", "Власов");
+        User student4 = new User("Александр", "Петрович", "Власов");
+        User student5 = new User("Петр", "Николаевич", "Николаев");
 
-        User student1 = new User("Ivan", "Ivanov", "Ivanovich");
-        User student2 = new User("Petr", "Petrov", "Petrovich");
-        User student3 = new User("Sidor", "Sidorov", "Sidorovich");
-
-//
-
-        groupController.createGroup("Electrics");
+        groupController.createGroup("2107");
         groupController.addStudentToGroup(student1);
         groupController.addStudentToGroup(student2);
         groupController.addStudentToGroup(student3);
-//
+        groupController.addStudentToGroup(student4);
+        groupController.addStudentToGroup(student5);
 
         UserComparator<UserInterface> comparator = new UserComparator<>();
         TreeSet<UserInterface> sortedGroup = new TreeSet<>(comparator);
 
-        for (User user : groupController.getGroup().getGroup()
-        ) {
+        for (User user : groupController.getGroup().getGroup()) {
             sortedGroup.add(user);
         }
         System.out.println(sortedGroup);
